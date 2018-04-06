d7a13244f161e909b1e4f3ceaccddc478b1faa39
Admin <vkeydixiomara@yahoo.com>
2018-04-06 14:14:11 -0600

    modificacion
    Modificar interfaz principal y menu

--- app/src/main/java/com/example/danny/kodax1/Principal.java
+++ app/src/main/java/com/example/danny/kodax1/Principal.java
@@ -49,11 +49,6 @@ ImageView ima ;
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-
-            return true;
-        }else
             if (id == R.id.contacto){
                 Intent i = new Intent(getApplicationContext(),Login.class);
                 startActivity(i);
